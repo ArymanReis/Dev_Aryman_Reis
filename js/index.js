54c7710e05a8373057b74c24eab3cacc0a608585
Aryman Reis <arymanreis@gmail.com>
2024-05-23 21:11:48 -0300

    mais um dia

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
 const arrowLeft      = document.querySelector("#arrow-left");
 const arrowRight     = document.querySelector("#arrow-right");
 
-const techs          = document.querySelectorAll(".tech");
 const techsContainer = document.querySelector("#tech-slide");
+
 
 techsContainer.addEventListener("wheel", (e) => {
     e.preventDefault();
     techsContainer.scrollLeft += e.deltaY;
+    techsContainer.style.scrollBehavior = "auto";
 });
 
 
 arrowLeft.addEventListener('click', () => {
-    techsContainer.scrollLeft -= 500;
+    techsContainer.style.scrollBehavior = "smooth";
+    techsContainer.scrollLeft -= 900;
 });
 
 arrowRight.addEventListener('click', () => {
-    techsContainer.scrollLeft += 500;
+    techsContainer.style.scrollBehavior = "smooth";
+    techsContainer.scrollLeft += 900;
 });
 
 
